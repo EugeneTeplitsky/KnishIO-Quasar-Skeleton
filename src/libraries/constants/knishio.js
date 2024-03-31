435e2f50fd1f89cf956ca9d6f96f4a5de15354f0
Eugene Teplitsky <eugene@wishknish.com>
2024-03-31 01:59:15 -0400

    Adding missing .env props to settings var

--- src/libraries/constants/knishio.js
+++ src/libraries/constants/knishio.js
@@ -1,6 +1,8 @@
 export const KNISHIO_SETTINGS = {
   salt: process.env.KNISHIO_SALT,
   cellSlug: process.env.KNISHIO_CELL_SLUG,
+  auth2faEnabled: process.env.KNISHIO_2FA_ENABLED,
+  admins: process.env.KNISHIO_APP_ADMINS,
   pubKeyField: process.env.KNISHIO_PUBKEY,
   encryptionToken: process.env.KNISHIO_ENCRYPTION_TOKEN,
   encryptionPosition: process.env.KNISHIO_ENCRYPTION_POSITION,
